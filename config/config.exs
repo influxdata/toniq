b50fc8f4f2957c92657435f47e60384fbebf6bf0
Joel Abshier <jabshier@influxdata.com>
2023-09-08 12:54:20 -0500

    remove exredis config

--- config/config.exs
+++ config/config.exs
@@ -17,5 +17,4 @@ end
 if Mix.env == :dev do
   # Running Toniq.TestWorker with the regular retry strategy is a bit too slow.
   config :toniq, retry_strategy: Toniq.RetryWithoutDelayStrategy
-  config :exredis, force_tls: false
 end
